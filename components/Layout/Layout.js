e4970fc5d861a1de850b51fbc952ec45be40e54d
Ernesto B <ejbcode@gmail.com>
2020-08-28 07:57:17 -0400

    .

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import { Global, css } from "@emotion/core";
+import Head from "next";
 
 const Layout = (props) => {
   return (
@@ -41,6 +42,20 @@ const Layout = (props) => {
           }
         `}
       />
+      <Head>
+        <html lang="es" />
+        <title>PH</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Header />
       <main>{props.children}</main>
     </>
